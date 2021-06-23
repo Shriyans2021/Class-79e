30abd2afbd4bb0883201634418839712b970570e
Shriyans2021 <81604852+Shriyans2021@users.noreply.github.com>
2021-06-22 20:10:43 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 nameofstudent = [];
 function submit()
 {
-    var number_1 = document.getElementById("student1").Value;
-    var number_2 = document.getElementById("student2").Value;
-    var number_3 = document.getElementById("student3").Value;
-    var number_4 = document.getElementById("student4").Value;
+    var number_1 = document.getElementById("student1").value;
+    var number_2 = document.getElementById("student2").value;
+    var number_3 = document.getElementById("student3").value;
+    var number_4 = document.getElementById("student4").value;
     console.log(number_1);
     console.log(number_2);
     console.log(number_3);
@@ -15,4 +15,12 @@ function submit()
     nameofstudent.push(number_4);
     document.getElementById("names").innerHTML = nameofstudent;
     console.log(nameofstudent);
+    document.getElementById("submit_button").style.display = "none";
+     document.getElementById("Sort_button").style.display = "inline-block";
+}
+function sort()
+{
+    nameofstudent.sort();
+     console.log(nameofstudent);
+      document.getElementById("names").innerHTML = nameofstudent;
 }
